ec29ebe34d4ae51a59e41c3e741f0d98181f8a93
AfsanaBrishty <afsanarain603@gmail.com>
2019-10-02 22:44:42 +0600

    Updated

--- src/austalumniassociationnb/MainFrame.java
+++ src/austalumniassociationnb/MainFrame.java
@@ -33,6 +33,7 @@ import javax.swing.table.TableModel;
 public class MainFrame extends javax.swing.JFrame implements DocumentListener{
     public MainFrame() {
         initComponents();
+        //combobox2.setVisible(false);
         Toolkit toolkit=getToolkit();
         Dimension size=toolkit.getScreenSize();
         setLocation(size.width/2 - getWidth()/2, size.height/2 - getHeight()/2);
@@ -50,6 +51,26 @@ public class MainFrame extends javax.swing.JFrame implements DocumentListener{
             {
                 findDept();
             }
+        /*if(combobox.getSelectedIndex()==3)
+            {
+              combobox2.setVisible(true);
+            }*/
+        if(combobox2.getSelectedIndex()==0)
+            {
+              AllCG();
+            }
+       if(combobox2.getSelectedIndex()==1)
+            {
+              AboveCG();
+            }
+       if(combobox2.getSelectedIndex()==2)
+            {
+              BelowCG();
+            }
+        /*if(combobox.getSelectedIndex()==3)
+            {
+             combobox2.setVisible(false);
+            }*/
 }
     
     public Connection getConnection()
@@ -260,7 +281,201 @@ public class MainFrame extends javax.swing.JFrame implements DocumentListener{
        MainAlumniTableJframe.setModel(model4);
        
     }
-   
+    public ArrayList<User> ListUsers4()
+    {
+        ArrayList<User> usersList5 = new ArrayList<User>();
+        
+        Statement st5;
+        ResultSet rs5;
+        if(combobox2.getSelectedIndex()==1)
+        {
+        try{
+            Connection con = getConnection();
+            st5 = con.createStatement();
+            String searchQuery4 = "SELECT * FROM MAINALUMNITABLE WHERE CGPA BETWEEN 3.00 and 4.00";
+            rs5= st5.executeQuery(searchQuery4);
+            
+            User user5;
+            
+            while(rs5.next())
+            {
+                user5 = new User(
+                                 rs5.getString("FullName"),
+                                 rs5.getInt("ID"),
+                                 rs5.getString("MailID"),
+                                 rs5.getString("ContactNo"),
+                                 rs5.getString("Address"),
+                                 rs5.getString("CGPA"),
+                                 rs5.getString("Gender"),
+                                 rs5.getString("Department"),
+                                 rs5.getString("YearOfAdmission"),
+                                 rs5.getString("PassingYear"),
+                                 rs5.getString("Password")
+                                );
+                usersList5.add(user5);
+            }
+            
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        }
+        
+        return usersList5;
+    }
+    public void AboveCG()
+    {
+        ArrayList<User> users5 = ListUsers4();
+        DefaultTableModel model5 = new DefaultTableModel();
+        model5.setColumnIdentifiers(new Object[]{"FullName","ID","MailID","ContactNo","Address","CGPA","Gender","Department","YearOfAdmission","PassingYear"});
+        Object[] row4 = new Object[10];
+        
+        for(int i = 0; i < users5.size(); i++)
+        {
+            row4[0] = users5.get(i).getFullName();
+            row4[1] = users5.get(i).getID();
+            row4[2] = users5.get(i).getMailID();
+            row4[3] = users5.get(i).getContactNo();
+            row4[4] = users5.get(i).getAddress();
+            row4[5] = users5.get(i).getCGPA();
+            row4[6] = users5.get(i).getGender();
+            row4[7] = users5.get(i).getDepartment();
+            row4[8] = users5.get(i).getYearOfAdmission();
+            row4[9] = users5.get(i).getPassingYear();
+            model5.addRow(row4);
+        }
+       MainAlumniTableJframe.setModel(model5);
+       
+    }
+    public ArrayList<User> ListUsers5()
+    {
+        ArrayList<User> usersList6 = new ArrayList<User>();
+        
+        Statement st6;
+        ResultSet rs6;
+        if(combobox2.getSelectedIndex()==0)
+        {
+        try{
+            Connection con = getConnection();
+            st6 = con.createStatement();
+            String searchQuery5 = "SELECT * FROM MAINALUMNITABLE";
+            rs6= st6.executeQuery(searchQuery5);
+            
+            User user6;
+            
+            while(rs6.next())
+            {
+                user6 = new User(
+                                 rs6.getString("FullName"),
+                                 rs6.getInt("ID"),
+                                 rs6.getString("MailID"),
+                                 rs6.getString("ContactNo"),
+                                 rs6.getString("Address"),
+                                 rs6.getString("CGPA"),
+                                 rs6.getString("Gender"),
+                                 rs6.getString("Department"),
+                                 rs6.getString("YearOfAdmission"),
+                                 rs6.getString("PassingYear"),
+                                 rs6.getString("Password")
+                                );
+                usersList6.add(user6);
+            }
+            
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        }
+        
+        return usersList6;
+    }
+    public void AllCG()
+    {
+        ArrayList<User> users6 = ListUsers5();
+        DefaultTableModel model6 = new DefaultTableModel();
+        model6.setColumnIdentifiers(new Object[]{"FullName","ID","MailID","ContactNo","Address","CGPA","Gender","Department","YearOfAdmission","PassingYear"});
+        Object[] row5 = new Object[10];
+        
+        for(int i = 0; i < users6.size(); i++)
+        {
+            row5[0] = users6.get(i).getFullName();
+            row5[1] = users6.get(i).getID();
+            row5[2] = users6.get(i).getMailID();
+            row5[3] = users6.get(i).getContactNo();
+            row5[4] = users6.get(i).getAddress();
+            row5[5] = users6.get(i).getCGPA();
+            row5[6] = users6.get(i).getGender();
+            row5[7] = users6.get(i).getDepartment();
+            row5[8] = users6.get(i).getYearOfAdmission();
+            row5[9] = users6.get(i).getPassingYear();
+            model6.addRow(row5);
+        }
+       MainAlumniTableJframe.setModel(model6);
+       
+    }
+  public ArrayList<User> ListUsers6()
+    {
+        ArrayList<User> usersList7 = new ArrayList<User>();
+        
+        Statement st7;
+        ResultSet rs7;
+        if(combobox2.getSelectedIndex()==2)
+        {
+        try{
+            Connection con = getConnection();
+            st7 = con.createStatement();
+            String searchQuery6 = "SELECT * FROM MAINALUMNITABLE WHERE CGPA<3.00";
+            rs7= st7.executeQuery(searchQuery6);
+            
+            User user7;
+            
+            while(rs7.next())
+            {
+                user7 = new User(
+                                 rs7.getString("FullName"),
+                                 rs7.getInt("ID"),
+                                 rs7.getString("MailID"),
+                                 rs7.getString("ContactNo"),
+                                 rs7.getString("Address"),
+                                 rs7.getString("CGPA"),
+                                 rs7.getString("Gender"),
+                                 rs7.getString("Department"),
+                                 rs7.getString("YearOfAdmission"),
+                                 rs7.getString("PassingYear"),
+                                 rs7.getString("Password")
+                                );
+                usersList7.add(user7);
+            }
+            
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        }
+        
+        return usersList7;
+    }
+    public void BelowCG()
+    {
+        ArrayList<User> users7 = ListUsers6();
+        DefaultTableModel model7 = new DefaultTableModel();
+        model7.setColumnIdentifiers(new Object[]{"FullName","ID","MailID","ContactNo","Address","CGPA","Gender","Department","YearOfAdmission","PassingYear"});
+        Object[] row6 = new Object[10];
+        
+        for(int i = 0; i < users7.size(); i++)
+        {
+            row6[0] = users7.get(i).getFullName();
+            row6[1] = users7.get(i).getID();
+            row6[2] = users7.get(i).getMailID();
+            row6[3] = users7.get(i).getContactNo();
+            row6[4] = users7.get(i).getAddress();
+            row6[5] = users7.get(i).getCGPA();
+            row6[6] = users7.get(i).getGender();
+            row6[7] = users7.get(i).getDepartment();
+            row6[8] = users7.get(i).getYearOfAdmission();
+            row6[9] = users7.get(i).getPassingYear();
+            model7.addRow(row6);
+        }
+       MainAlumniTableJframe.setModel(model7);
+       
+    }
     FUllView2 jtRowData = new FUllView2();  //Je Frame e dekhabe Row click er por//
     
     //static FullViewofDataFrame jtRowData2 = new FullViewofDataFrame();
@@ -318,6 +533,7 @@ public class MainFrame extends javax.swing.JFrame implements DocumentListener{
         combobox = new javax.swing.JComboBox<>();
         jLabel4 = new javax.swing.JLabel();
         searchdata = new javax.swing.JTextField();
+        combobox2 = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -350,7 +566,7 @@ public class MainFrame extends javax.swing.JFrame implements DocumentListener{
             }
         });
 
-        combobox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "ID", "Name", "Department" }));
+        combobox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "ID", "Name", "Department", "CGPA" }));
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 3, 12)); // NOI18N
         jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -359,6 +575,13 @@ public class MainFrame extends javax.swing.JFrame implements DocumentListener{
         searchdata.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyReleased(java.awt.event.KeyEvent evt) {
                 searchdataKeyReleased(evt);
+            }
+        });
+
+        combobox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "All", "Above 3.00(including 3.00)", "Below 3.00" }));
+        combobox2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                combobox2ActionPerformed(evt);
             }
         });
 
@@ -382,7 +605,9 @@ public class MainFrame extends javax.swing.JFrame implements DocumentListener{
                                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(combobox, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(18, 18, 18)
-                                .addComponent(searchdata, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(searchdata, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(combobox2, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE))))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -398,7 +623,9 @@ public class MainFrame extends javax.swing.JFrame implements DocumentListener{
                             .addComponent(combobox, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(searchdata, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(39, 39, 39)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(combobox2, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 394, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(back_btn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -460,13 +687,24 @@ public class MainFrame extends javax.swing.JFrame implements DocumentListener{
             }
         /*if(combobox.getSelectedIndex()==3)
             {
-                findName();
-            }
-        if(combobox.getSelectedIndex()==4)
-            {
-                findName();
+              combobox2.setVisible(true);
             }*/
     }//GEN-LAST:event_searchdataKeyReleased
+
+    private void combobox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_combobox2ActionPerformed
+           if(combobox2.getSelectedIndex()==0)
+            {
+              AllCG();
+            }
+       if(combobox2.getSelectedIndex()==1)
+            {
+              AboveCG();
+            }
+       if(combobox2.getSelectedIndex()==2)
+            {
+              BelowCG();
+            }
+    }//GEN-LAST:event_combobox2ActionPerformed
 
     public static void main(String args[]) {
             }
@@ -475,6 +713,7 @@ public class MainFrame extends javax.swing.JFrame implements DocumentListener{
     private javax.swing.JTable MainAlumniTableJframe;
     private javax.swing.JButton back_btn;
     private javax.swing.JComboBox<String> combobox;
+    private javax.swing.JComboBox<String> combobox2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
